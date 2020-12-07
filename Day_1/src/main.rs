7a94fdf0c49c2277141549fe1268ae0ebe4aa9ab
texhnolainx <texhnolainx@gmail.com>
2020-12-07 19:34:32 +0100

    Day_1 [Rust]

---
+++ Day_1/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+const FILE_NAME: &str = "dataset";
+
+fn main () {
+
+    let dataset: String = fs::read_to_string(FILE_NAME).expect("Something went wrong...");
+
+    // PART 1
+    let mut floor: i16 = 0;
+    let mut not_found: bool = true;
+    for (i, c) in dataset.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => ()
+        }
+        if floor < 0 && not_found {
+            println!("Basement @ {}.", i + 1);
+            not_found = false;
+        }
+    }
+    println!("{}", floor);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# AoC2015
+
+My solutions to the Advent of Code 2015 problems.
+*Mainly in Rust.*
